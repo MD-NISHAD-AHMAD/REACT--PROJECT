1d81f8c4a8bacb3424b9aaace5a1ac165ab9a58d
MD-NISHAD-AHMAD <nishadahmadm@gmail.com>
2026-03-02 20:04:50 +0530

    brand page

---
+++ Brand-Page/src/Components/Navigation.jsx
@@ -0,0 +1,18 @@
+const Navigation = () => {
+    return(
+        <nav className="container">
+        <div className="logo">
+          <img src="../public/images/nike-logo-49336.png" alt="logo" />
+        </div>
+        <ul>
+          <li href="#">MENU</li>
+          <li href="#">LOCATION</li>
+          <li href="#">ABOUT</li>
+          <li href="#">CONTACT</li>
+        </ul>
+        <button>Login</button>
+       </nav>
+    );
+};
+
+export default Navigation;
